d3c280c41d625021f235820004b5ce9977ee694a
Gabriel <morenna293@gmail.com>
2026-06-05 16:15:30 -0300

    pulo

--- Assets/Script/PersonagemController.cs
+++ Assets/Script/PersonagemController.cs
@@ -8,18 +8,32 @@ public class PersonagemController : MonoBehaviour
 
     public float vel;
 
+    public float jumpForce;
+
+    public GameObject groundCheck;
+
+    private GroundCheck groundCheckScript; 
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = this.GetComponent<Rigidbody2D>();
+        groundCheckScript = groundCheck.GetComponent<GroundCheck>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        if (rb2d.velocity.magnitude < 5) {
+        if (rb2d.velocity.magnitude < 5)
+         {
         rb2d.velocity += new Vector2(vel,0)*horizontalInput*Time.deltaTime;
           }
+
+        if(Input.GetKey(KeyCode.Space) && groundCheckScript.isOnGround)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+        }
+
     }
 }
